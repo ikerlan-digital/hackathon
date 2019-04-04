547bbf15b6e716e405d0ef104a301cdc81cddd92
Marco Gonzalez Hierro <marco.gonzalez@ikerlan.es>
2019-04-04 09:19:11 +0200

    Added hex to show hex data

--- examples/sensor-node-serial/python/hackathon.py
+++ examples/sensor-node-serial/python/hackathon.py
@@ -3,5 +3,5 @@ import serial
 if __name__ == "__main__":
     with serial.Serial('/dev/ttyS0', 9600, timeout=1) as ser:
         data = ser.read(200)
-        print(data)
+        print(data.hex())
         ser.close()
